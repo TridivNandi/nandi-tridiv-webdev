ecd6d402bfeb09edd6886d1ffca0cf2482d4f0ee
tridivnandi <nandi.t@husky.neu.edu>
2017-04-13 11:17:38 -0400

    Removing Comment
    Removing Comment

--- webdev/project/model/user/user.schema.server.js
+++ webdev/project/model/user/user.schema.server.js
@@ -19,7 +19,6 @@ module.exports = function (mongoose) {
         following: [{type: mongoose.Schema.Types.ObjectId, ref: 'RateMyMovieUser'}],
         role: {type: String, enum: ['user', 'admin'], default: 'user'},
         dateCreated: {type: Date, default: Date.now()},
-        //type: {type: String, default: 'mr'}
     }, {collection: 'rate_my_movies.user'});
     return userSchema;
 
